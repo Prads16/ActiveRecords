f1c0999f2cd179fe9f9abc5efda0d223a8c1b4a6
Pradnya R Ambre <pra22@afsconnect1.njit.edu>
2017-11-19 14:10:18 -0500

    displaying table data in array

--- index.php
+++ index.php
@@ -1,26 +1,30 @@
 <?php
- define('DATABASE', 'pra22');
- define('USERNAME', 'pra22');
- define('PASSWORD', 'zsLR8d2wM');
- define('CONNECTION', 'sql2.njit.edu');
 
- class dbConnect
- {
- protected static $conn;
- private function __construct()
+ini_set('display_errors','On');
+error_reporting(E_ALL);
+
+define('DATABASE', 'pra22');
+define('USERNAME', 'pra22');
+define('PASSWORD', 'zsLR8d2wM');
+define('CONNECTION', 'sql2.njit.edu');
+
+class dbConnect
+{
+  protected static $conn;
+  private function __construct()
   {
     try 
     {
       self::$conn = new PDO('mysql:host=' . CONNECTION . ';dbname=' . DATABASE, USERNAME, PASSWORD);
       self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      echo 'Database connected successfully. <br>';
+      echo '<center><b>Database connected successfully.</b></center> <br>';
     } 
     catch (PDOException $e) 
     {
       echo 'Connection to the database failed ' . $e->getMessage() . '<br>';
     }
   }
- public static function getConnection()
+  public static function getConnection()
   {
     if (!self::$conn)
     {
@@ -28,5 +32,54 @@
     }
     return self::$conn;
   }
- }
+
+}
+
+class collection 
+{
+  protected $html;
+  public static function createdb() 
+  {
+    $model = new static::$modelName;
+    return $model;
+  }
+  public static function findAll()
+  {
+    $db = dbConnect::getConnection();
+    $tableName = get_called_class();
+    $sqlquery = 'SELECT * FROM ' . $tableName;
+    $statement = $db->prepare($sqlquery);
+    $statement->execute();
+    $childclass = static::$modelName;
+    $statement->setFetchMode(PDO::FETCH_CLASS, $childclass);
+    $recordsSet =  $statement->fetchAll();
+    return $recordsSet;
+  }
+  public static function findOne($id) 
+  {
+    $db = dbConnect::getConnection();
+    $tableName = get_called_class();
+    $sqlquery = 'SELECT * FROM ' . $tableName . ' WHERE id =' . $id;
+    $statement = $db->prepare($sqlquery);
+    $statement->execute();
+    $childclass = static::$modelName;
+    $statement->setFetchMode(PDO::FETCH_CLASS, $childclass);
+    $recordsSet =  $statement->fetchAll();
+    return $recordsSet[0];
+  }
+}
+class accounts extends collection 
+{
+  protected static $modelName = 'account';
+}
+class todos extends collection 
+{
+  protected static $modelName = 'todo';
+}
+ 
+$records = accounts::findAll();
+print_r($records);
+$records = todos::findAll();
+print_r($records);
+ 
  ?>
